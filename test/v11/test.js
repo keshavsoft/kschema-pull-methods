77d61e39c6c17ee31b0523ea3140aef3b79aeee5
keshavsoft <nknnkumar@yahoo.com>
2026-07-28 13:49:40 +0530

    9 forRestClientFull

---
+++ test/v11/test.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from 'path';
+
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const appJsPath = path.join(__dirname, "api", "v1", "doctors", "end-points.js");
+const fileContent = fs.readFileSync(appJsPath, 'utf8');
+
+import defaultFunc from '../../index.js';
+import extractRegex from './extractRegex.js';
+
+const k1 = defaultFunc({
+    filePath: appJsPath, fileContent,
+    inTargetPath: __dirname,
+    extractRegex: extractRegex.fromEndPointsJs
+});
+
+// console.log("ssssssssss : ", k1);
+console.log("ssssssssss : ", k1);
